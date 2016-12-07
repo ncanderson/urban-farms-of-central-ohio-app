a6761abf81d7af4d065423e4181b18ffd9b77ca4
Christian Armstrong <christian@medpactech.com>
2016-12-07 09:46:33 -0500

    db working for users

--- database/create-capstone-db.sh
+++ database/create-capstone-db.sh
@@ -4,4 +4,4 @@ psql -U postgres -f "$BASEDIR/dropdb.sql" &&
 createdb -U postgres capstone &&
 psql -U postgres -d capstone -f "$BASEDIR/schema.sql" &&
 psql -U postgres -d capstone -f "$BASEDIR/user.sql" &&
-psql -U postgres -d capstone -f "$BASEDIR/data.sql"
+psql -U postgres -d capstone -f "$BASEDIR/data-development.sql"
--- database/schema.sql
+++ database/schema.sql
@@ -16,86 +16,88 @@ CREATE TABLE buyer_information (
   contact_phone varchar(10) NULL,	  -- 10 digit phone number without special characters
   notes varchar (250) NULL, 		  -- Optional notes field
   is_active boolean NOT NULL,		  -- True for active buyers
-  description varchar(150) NULL,  -- Description of buyer
-  PRIMARY KEY (buyer_id)
+  description varchar(150) NULL,  	  -- Description of buyer
+  apply_large_order_discount boolean NOT NULL,  -- Boolean to determain if buyer gets discount for orders >$150
+  discount decimal(3,2) NULL constraint chk_buyer_information_discount check (discount between 0 and 1) 		  -- Discount value stored as X.XX ie 50% is stored as 0.50
 );
 
 CREATE TABLE credentials (
   credentials_id serial PRIMARY KEY,
-  password varchar(25) NOT NULL,
+  password varchar(25) NOT NULL
 );
 
 CREATE TABLE contact_preference_type (
   contact_preference_type_id serial PRIMARY KEY,
   contact_preference_type_name varchar(6) NOT NULL,
-  contact_preference_type_description varchar(150) NULL,
-  PRIMARY KEY (contact_preference_type_id)
+  contact_preference_type_description varchar(150) NULL
 );
 
 CREATE TABLE user_type (
   user_type_id serial PRIMARY KEY,
   user_type_name varchar(15) NOT NULL,
-  user_type_description varchar(150) NULL,
+  user_type_description varchar(150) NULL
 );
 
 CREATE TABLE sale_type (
   sale_type_id serial PRIMARY KEY,
   sale_type_name varchar(25) NOT NULL,
-  sale_type_description varchar(150) NULL,
+  sale_type_description varchar(150) NULL
 );
 
 CREATE TABLE invoice_status (
   invoice_status_id serial PRIMARY KEY,
   invoice_status_name varchar(25) NOT NULL,
-  invoice_status_description varchar(150) NULL,
+  invoice_status_description varchar(150) NULL
 );
 
-CREATE TABLE user (
+CREATE TABLE users (
   user_id serial PRIMARY KEY,
   email varchar(100) NOT NULL, -- email is username 
-  user_type_id integer(2) NOT NULL REFERENCES user_type,
-  buyer_id integer(10) NULL REFERENCES buyer_information,
-  credentials_id integer(10) NOT NULL REFERENCES credentials,
+  user_type_id integer NOT NULL REFERENCES user_type,
+  buyer_id integer NULL REFERENCES buyer_information,
+  credentials_id integer NOT NULL REFERENCES credentials,
   first_name varchar(80) NOT NULL,
   last_name varchar(80) NOT NULL,
-  contact_preference_type_id integer(2) NULL REFERENCES contact_preference_type,
+  contact_preference_type_id integer NULL REFERENCES contact_preference_type,
   user_phone_number varchar(10) NULL,
   is_global_admin boolean NOT NULL,
   is_admin boolean NOT NULL,
-  is_active boolean NOT NULL,
+  is_active boolean NOT NULL
 );
 
-CREATE TABLE item (
-  item_id serial PRIMARY KEY,
-  item_type varchar(25) NOT NULL,
-  item_variety varchar(25) NULL,
-  item_image_id varchar(50) NULL,
-  item_description varchar(150) NULL,
-);
+-- CREATE TABLE item (
+--   item_id serial PRIMARY KEY,
+--   item_type varchar(25) NOT NULL,
+--   item_variety varchar(25) NULL,
+--   item_image_id varchar(50) NULL,
+--   item_description varchar(150) NULL
+-- );
 
-CREATE TABLE item_price (
-  item_price_id serial PRIMARY KEY,
-  item_id integer(10) NOT NULL,
-  date_added date NOT NULL,
-  sale_type_id integer(3) NOT NULL,
-  item_price money NOT NULL,
-);
+-- CREATE TABLE item_price (
+--   item_price_id serial PRIMARY KEY,
+--   item_id integer NOT NULL,
+--   date_added timestamp NOT NULL,
+--   sale_type_id integer NOT NULL,
+--   item_price money NOT NULL
+-- );
 
-CREATE TABLE invoice_item (
-  invoice_item_id serial PRIMARY KEY,
-  invoice_id integer(10) NOT NULL REFERENCES invoice,
-  invoice_quantity integer(5) NOT NULL,
-  item_harvest_details_id integer(2) NOT NULL REFERENCES harvest_details,
-  item_price_id integer(3) NOT NULL REFERENCES item_price,
-  );
+-- CREATE TABLE invoice (
+--   invoice_id serial PRIMARY KEY,
+--   invoice_date timestamp NOT NULL,
+--   user_id integer NOT NULL REFERENCES users,
+--   buyer_id integer NOT NULL REFERENCES buyer_information,
+--   sale_type_id integer NOT NULL,
+--   invoice_status_id integer NOT NULL REFERENCES invoice_status
+-- );
 
-CREATE TABLE invoice (
-  invoice_id serial PRIMARY KEY,
-  invoice_date date NOT NULL,
-  user_id integer(10) NOT NULL REFERENCES user,
-  buyer_id integer(10) NOT NULL REFERENCES buyer_information,
-  sale_type_id integer(3) NOT NULL,
-  invoice_status_id integer(3) NOT NULL REFERENCES invoice_status
-);
+-- CREATE TABLE invoice_item (
+--   invoice_item_id serial PRIMARY KEY,
+--   invoice_id integer NOT NULL REFERENCES invoice,
+--   invoice_quantity integer NOT NULL,
+--   item_harvest_details_id integer NOT NULL REFERENCES harvest_details,
+--   item_price_id integer NOT NULL REFERENCES item_price,
+--   invoice_date timestamp with time zone NOT NULL
+--   );
+
 
 COMMIT;
