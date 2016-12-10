dda60735235f6f9ca3d822be8e55d700e6db3a0c
Christian Armstrong <christian@medpactech.com>
2016-12-10 12:20:43 -0500

    update legacy data with new changes from meeting with product owner

--- database/schema.sql
+++ database/schema.sql
@@ -100,14 +100,11 @@ CREATE TABLE item_harvest_details (
   harvest_availability varchar(150) NULL,
   harvest_details_comments varchar(150) NULL,
   harvest_date timestamp NOT NULL,
-<<<<<<< HEAD
   item_price decimal NOT NULL,
   harvest_end_date timestamp NULL,
   pick_list_comments varchar(250) NULL,
-  is_bulk boolean NOT NULL DEFAULT FALSE
-=======
+  is_bulk boolean NOT NULL DEFAULT FALSE,
   harvest_reconciliation_id integer NULL REFERENCES harvest_reconciliation
->>>>>>> 72489848449339d9a226717ab58f7f19e53f277b
 );
 
 CREATE TABLE invoice_item (
@@ -115,15 +112,12 @@ CREATE TABLE invoice_item (
   invoice_id integer NOT NULL REFERENCES invoice,
   invoice_quantity integer NOT NULL,
   item_harvest_details_id integer NOT NULL REFERENCES item_harvest_details,
-<<<<<<< HEAD
   update_item_price decimal NULL,
-  invoice_date timestamp NOT NULL,
   user_id integer NOT NULL,
-  invoice_item_status_id integer NOT NULL DEFAULT  REFERENCES invoice_status
-=======
-  item_price_id integer NOT NULL REFERENCES item_price,
-  added_to_invoice_date timestamp NOT NULL
->>>>>>> 72489848449339d9a226717ab58f7f19e53f277b
-  );
+  invoice_item_status_id integer NOT NULL REFERENCES invoice_status,
+  item_price_id integer NULL, -- REFERENCES item_price,  not sure how to handle this?
+  added_to_invoice_date timestamp NOT NULL,
+  item_id integer NOT NULL
+);
 
 COMMIT;
