789c6d75986d73c9d4a27b155807f9fdc39cef10
CHRISTIAN\Christian <christian@medpactech.com>
2016-12-10 13:48:39 -0500

    refactor database

---
+++ database/users-development.sql
@@ -0,0 +1,9 @@
+-- Insert Development Users
+BEGIN;
+
+INSERT INTO users (user_id, email, user_type, credentials_id, first_name, last_name, is_global_admin, is_admin, is_active) VALUES (0, 'some@archive.com', 0, 1, 'Archive', 'User', 'false', 'false', 'true');
+INSERT INTO users (user_id, email, user_type, credentials_id, first_name, last_name, is_global_admin, is_admin, is_active) VALUES (1, 'some@admin.com', 0, 1, 'Admin', 'Joe', 'true', 'false', 'true');
+INSERT INTO users (user_id, email, user_type, credentials_id, first_name, last_name, is_global_admin, is_admin, is_active) VALUES (2, 'some@farmer.com', 1, 2, 'Farmer', 'Joe', 'false', 'true', 'true');
+INSERT INTO users (user_id, email, user_type, credentials_id, first_name, last_name, is_global_admin, is_admin, is_active) VALUES (3, 'some@buyer.com', 2, 3, 'Buyer', 'Joe', 'false', 'false', 'true');
+
+COMMIT;
