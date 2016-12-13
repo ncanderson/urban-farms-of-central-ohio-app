d5faf55be2cabc8fdd5688cc713bbd40def78b65
Christian Armstrong <christian@medpactech.com>
2016-12-13 10:08:23 -0500

    Misc SQL queries.

--- database/sql-querys-to-save.sql
+++ database/sql-querys-to-save.sql
@@ -8,15 +8,15 @@ UPDATE item_harvest_details SET harvest_quantity = 0;
 COMMIT;
 
 
- SELECT item_harvest_details_id, harvest_quantity, item_price FROM item_harvest_details WHERE harvest_quantity > 0;
+ SELECT item_harvest_details_id, harvest_quantity, average_size_of_item, item_price FROM item_harvest_details WHERE harvest_quantity > 0;
 
-
-SELECT item_id, item_price, max(harvest_date) as maxDate
-FROM item_harvest_details
-WHERE item_id = 5;
-
-SELECT item_price, max(harvest_date) as max_date
+SELECT item_price, harvest_date
 FROM item_harvest_details
 WHERE item_id = 5
-GROUP BY item_price
+ORDER BY harvest_date desc
 LIMIT 1;
+
+ SELECT * FROM item_harvest_details WHERE harvest_quantity > 0;
+
+ SELECT item_harvest_details_id as ID, item_id as Item, harvest_quantity as Qty, harvest_image_id as Img, average_size_of_item as Size, harvest_availability as Avail, harvest_details_comments as Notes, harvest_date as Date, item_price as Price, harvest_end_date as End, pick_list_comments as Pick, is_bulk, harvest_reconciliation_id as Rec FROM item_harvest_details WHERE harvest_quantity > 0;
+
